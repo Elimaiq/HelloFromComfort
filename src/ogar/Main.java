13f041c627efe2f388e7828b9874db71dc883c4f
Comfort <elimaiq@gmail.com>
2021-05-20 22:59:21 +0100

    CS 246 Prove04

---
+++ src/ogar/Main.java
@@ -0,0 +1,9 @@
+package ogar;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, my name is Comfort Ogar.");
+    }
+}
